406f161759a92766d0643c64ba8b42e7bd6e224f
fadhil <134047249+FadhilMulinya@users.noreply.github.com>
2025-05-29 19:34:31 +0300

    updating the routers

--- V1/LoanContract.sol
+++ V1/LoanContract.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-import {SafeTransferLib} from "https://github.com/Vectorized/solady/blob/main/src/utils/SafeTransferLib.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol" ;
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {SafeTransferLib} from "lib/solady/src/utils/SafeTransferLib.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol" ;
+import {IERC20} from "lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
 
 contract Loan {
 
